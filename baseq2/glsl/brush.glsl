87e74924fa4fc1d1d428cd08d1466f9e6030cf43
Pat Raynor <raynorpat@gmail.com>
2017-05-18 18:19:44 -0400

    Commit of GLSL shaders

---
+++ baseq2/glsl/brush.glsl
@@ -0,0 +1,43 @@
+
+// common
+uniform float scroll;
+
+INOUTTYPE vec4 texcoords[2];
+
+#ifdef VERTEXSHADER
+uniform mat4 localMatrix;
+
+layout(location = 0) in vec4 position;
+layout(location = 1) in vec4 diffuse;
+layout(location = 2) in vec4 lightmap;
+
+
+void LightmappedVS ()
+{
+	gl_Position = localMatrix * position;
+	texcoords[0] = diffuse + (vec4 (scroll, 0, 0, 0) * diffuse.z);
+	texcoords[1] = lightmap;
+}
+#endif
+
+
+#ifdef FRAGMENTSHADER
+#extension GL_EXT_texture_array : enable
+
+uniform sampler2D diffuse;
+uniform sampler2DArray lightmap;
+uniform float surfalpha;
+uniform mat4 colormatrix;
+
+out vec4 fragColor;
+
+
+void LightmappedFS ()
+{
+	vec4 lmap = texture2DArray (lightmap, texcoords[1].xyz);
+	vec4 final = texture (diffuse, texcoords[0].st) * (colormatrix * (lmap / lmap.a));
+
+	fragColor = vec4 (final.rgb, surfalpha);
+}
+#endif
+
