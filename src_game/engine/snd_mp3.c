4b962930926e077ab03ede76499e8597df13a367
Pat Raynor <raynorpat@gmail.com>
2017-05-27 08:15:56 -0400

    Get rid of this rudimentary mp3 check

--- src_game/engine/snd_mp3.c
+++ src_game/engine/snd_mp3.c
@@ -376,25 +376,6 @@ static void S_MP3_CodecShutdown (void)
 {
 }
 
-static int MP3_check_file (snd_stream_t *stream)
-{
-	unsigned char magic[16];
-
-	if (FS_fread(magic, 1, 16, &stream->fh) != 16)
-		return -1;
-	if ((magic[0] == 0xFF && (magic[1] & 0xF0) == 0xF0) ||
-	    tag_is_id3v2(magic, 16))
-		return 0;
-	if (FS_fseek(&stream->fh, -128, SEEK_END) < 0)
-		return -1;
-	if (FS_fread(magic, 1, 16, &stream->fh) != 16)
-		return -1;
-	if (tag_is_id3v1(magic, 16))
-		return 0;
-
-	return -1;
-}
-
 static snd_stream_t *S_MP3_CodecOpenStream (char *filename)
 {
 	snd_stream_t *stream;
@@ -404,20 +385,6 @@ static snd_stream_t *S_MP3_CodecOpenStream (char *filename)
 		return NULL;
 
 	stream->priv = Z_Malloc(sizeof(mp3_priv_t));
-
-	/* check some rudimentary info before feeding it to libmad. */
-	if (MP3_check_file(stream) < 0)
-	{
-		/*
-		Com_Printf("%s format couldn't be identified.\n", filename);
-		Z_Free(stream->priv);
-		S_CodecUtilClose(&stream);
-		return NULL;
-		*/
-		Com_DPrintf("%s has no identifier tags.\n", filename);
-	}
-
-	FS_rewind(&stream->fh);
 
 	if (mp3_startread(stream) < 0)
 	{
