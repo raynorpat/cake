158b1f41cee2ba17dd32b9cf133072b8b07fb8c4
Pat Raynor <raynorpat@gmail.com>
2017-11-19 18:49:18 -0500

    Not really gl specific

--- src_main/refresh/matrix.c
+++ src_main/refresh/matrix.c
@@ -17,10 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 */
-// matrix.c -- opengl matrix math functionality
-
-// TODO: split that out into matrix.h
-#include "gl/gl_local.h"
+// matrix.c -- matrix math functionality
+#include "ref_public.h"
 
 inline float QXVec3Length(const QXVECTOR3 *pv)
 {
