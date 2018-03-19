ff013f5368d61b2af07abc1e1c8d0e0af85178d9
Pat Raynor <raynorpat@gmail.com>
2018-03-18 21:40:35 -0400

    Update UWP cmake batch scripts

--- src_main/cmake-msvc2017-64bit-uwp.bat
+++ src_main/cmake-msvc2017-64bit-uwp.bat
@@ -1,5 +1,5 @@
 del /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 15 Win64" -DCMAKE_SYSTEM_NAME="WindowsStore" -DCMAKE_SYSTEM_VERSION="10.0" -DWIN_UWP=ON -DMONOLITH=ON ..
+cmake -G "Visual Studio 15 Win64" -DCMAKE_SYSTEM_NAME="WindowsStore" -DCMAKE_SYSTEM_VERSION="10.0" -DMONOLITH=ON -DWIN_UWP=ON ..
 pause
