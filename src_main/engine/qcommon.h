0873afedb97182a4d58aa946e65f60dff1cc0677
Pat Raynor <raynorpat@gmail.com>
2018-03-22 16:41:31 -0400

    Set version to 0.09

--- src_main/engine/qcommon.h
+++ src_main/engine/qcommon.h
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 // qcommon.h -- definitions common between client and server, but not game.dll
 #include "q_shared.h"
 
-#define	VERSION		3.21
+#define	VERSION		0.09
 
 #define	BASEDIRNAME	"baseq2"
 
