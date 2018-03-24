761e78224b01a611034a04f228ac587638d1c8e5
Pat Raynor <raynorpat@gmail.com>
2018-03-24 15:40:57 -0400

    Default rate to 10000

--- src_main/engine/cl_main.c
+++ src_main/engine/cl_main.c
@@ -1519,7 +1519,7 @@ void CL_InitLocal (void)
 	info_spectator = Cvar_Get ("spectator", "0", CVAR_USERINFO);
 	name = Cvar_Get ("name", "unnamed", CVAR_USERINFO | CVAR_ARCHIVE);
 	skin = Cvar_Get ("skin", "male/grunt", CVAR_USERINFO | CVAR_ARCHIVE);
-	rate = Cvar_Get ("rate", "25000", CVAR_USERINFO | CVAR_ARCHIVE);	// FIXME
+	rate = Cvar_Get ("rate", "10000", CVAR_USERINFO | CVAR_ARCHIVE);
 	msg = Cvar_Get ("msg", "1", CVAR_USERINFO | CVAR_ARCHIVE);
 	hand = Cvar_Get ("hand", "0", CVAR_USERINFO | CVAR_ARCHIVE);
 	fov = Cvar_Get ("fov", "90", CVAR_USERINFO | CVAR_ARCHIVE);
