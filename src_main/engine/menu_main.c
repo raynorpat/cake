fa677f0e4be4d8b8568b7227a3b5920a5f1fa8d4
Pat Raynor <raynorpat@gmail.com>
2018-04-12 15:00:05 -0400

    Added #D Quad Damage model to replace the multiple sprite images on the main menu

--- src_main/engine/menu_main.c
+++ src_main/engine/menu_main.c
@@ -29,13 +29,14 @@ MAIN MENU
 =======================================================================
 */
 
+float SCR_CalcFovY (float fov_x, float width, float height);
+
 extern qboolean m_entersound;
 
+#define QUAD_CURSOR_MODEL "models/ui/quad_cursor.md2"
 static int m_main_cursor;
-#define NUM_CURSOR_FRAMES 15
 
 #define	MAIN_ITEMS	5
-
 static char *names[] =
 {
 	"m_main_game",
@@ -63,26 +64,59 @@ x,y. The pic will extend to the left of x,
 and both above and below y.
 =============
 */
-static void M_DrawCursor(int x, int y, int f)
+static void M_DrawCursor(int x, int y)
 {
-	char cursorname[80];
 	static qboolean cached;
+	refdef_t		refdef;
+	entity_t		quadEnt, *ent;
+	float			rx, ry, rw, rh;
+	int				yaw;
 
 	if (!cached)
 	{
-		int i;
+		struct model_s *quadModel;
+		quadModel = RE_RegisterModel (QUAD_CURSOR_MODEL);
 
-		for (i = 0; i < NUM_CURSOR_FRAMES; i++)
-		{
-			Com_sprintf(cursorname, sizeof(cursorname), "m_cursor%d", i);
-			RE_Draw_RegisterPic(cursorname);
-		}
-
-		cached = true;
+		cached = (quadModel != NULL);
 	}
 
-	Com_sprintf(cursorname, sizeof(cursorname), "m_cursor%d", f);
-	SCR_DrawPic (x, y, 24, 24, cursorname);
+	yaw = anglemod(cl.time / 10);
+
+	memset(&refdef, 0, sizeof(refdef));
+	memset(&quadEnt, 0, sizeof(quadEnt));
+
+	// cursor is 24 x 34
+	rx = x;
+	ry = y;
+	rw = 24;
+	rh = 34;
+	SCR_AdjustFrom640 (&rx, &ry, &rw, &rh);
+
+	refdef.x = rx;
+	refdef.y = ry;
+	refdef.width = rw;
+	refdef.height = rh;
+	refdef.fov_x = 40;
+	refdef.fov_y = SCR_CalcFovY (refdef.fov_x, refdef.width, refdef.height);
+	refdef.time = cls.realtime * 0.001;
+	refdef.areabits = 0;
+	refdef.lightstyles = 0;
+	refdef.rdflags = RDF_NOWORLDMODEL;
+	refdef.num_entities = 0;
+	refdef.entities = &quadEnt;
+
+	ent = &quadEnt;
+	ent->model = RE_RegisterModel (QUAD_CURSOR_MODEL);
+	ent->flags = RF_FULLBRIGHT | RF_NOSHADOW | RF_DEPTHHACK;
+	Vector3Set (ent->currorigin, 40, 0, -18);
+	VectorCopy (ent->currorigin, ent->lastorigin);
+	ent->currframe = 0;
+	ent->lastframe = 0;
+	ent->backlerp = 0.0;
+	ent->angles[1] = yaw;
+	refdef.num_entities++;
+
+	RE_RenderFrame (&refdef);
 }
 
 static void MainMenu_CursorDraw(void *self)
@@ -90,7 +124,7 @@ static void MainMenu_CursorDraw(void *self)
 	menubitmap_s *b;
 
 	b = (menubitmap_s *)self;
-	M_DrawCursor(b->generic.x - 25, b->generic.y + 5, (int)(cls.realtime / 100) % NUM_CURSOR_FRAMES);
+	M_DrawCursor(b->generic.x - 25, b->generic.y + 5);
 }
 
 void M_Main_Draw (menuframework_s *self)
