e0e346867429588832c16ffba84b057229a816b7
Pat Raynor <raynorpat@gmail.com>
2018-04-13 16:33:42 -0400

    Fix warning when doing a vid_restart about r_avilableModes not being able to be modified

--- src_main/refresh/vid/vid_gl.c
+++ src_main/refresh/vid/vid_gl.c
@@ -156,7 +156,7 @@ static void VID_DetectAvailableModes(void)
 	{
 		buf[strlen(buf) - 1] = 0;
 		VID_Printf(PRINT_ALL, "Available modes: '%s'\n", buf);
-		Cvar_Set("r_availableModes", buf);
+		Cvar_ForceSet ("r_availableModes", buf);
 	}
 	SDL_free(modes);
 }
@@ -229,9 +229,7 @@ vidrserr_t VID_InitWindow(int mode, int fullscreen)
 	{
 		display = SDL_GetWindowDisplayIndex(window);
 		if (display < 0)
-		{
 			VID_Printf(PRINT_DEVELOPER, S_COLOR_RED "SDL_GetWindowDisplayIndex() failed: %s\n", SDL_GetError());
-		}
 	}
 
 	if (display >= 0 && SDL_GetDesktopDisplayMode(display, &desktopMode) == 0)
